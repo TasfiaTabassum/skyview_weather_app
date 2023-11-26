9d091bb646183ebe5e23c4905bce5fa944b3a6bd
Tasfia Tabassum <58984231+TasfiaTabassum@users.noreply.github.com>
2023-11-26 10:15:39 +0600

    Update constants.dart

--- lib/constants/constants.dart
+++ lib/constants/constants.dart
@@ -1,5 +1,11 @@
-const base_url = "http://api.weatherapi.com/v1/forecast.json?key=3059d01461844c6bbea91547232509 ";
+
+// paste the API LINK of a Weather API
+
+const base_url = "API LINK ";
+
+
 
 // String API_KEY = "104df3387706329c75b1d4377f34217f";
 
 // String API_KEY = "AIzaSyCNr9CVVLCT2uFyemZpvlOFdmY3wjKBby0";
+// paste the API LINK of a Weather API
